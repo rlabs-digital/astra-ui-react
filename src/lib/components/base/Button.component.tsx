520714844272b408939c503f8de0867fb68b8418
iamrishupatel <rishupatel04@gmail.com>
2023-05-09 18:45:16 +0530

    setup husky with eslint

--- src/lib/components/base/Button.component.tsx
+++ src/lib/components/base/Button.component.tsx
@@ -1,7 +1,7 @@
 import { UnStyledButtonProps } from '$types/Button';
-import { FC } from 'react';
+import React from 'react';
 
-const Button: FC<UnStyledButtonProps> = ({ children, ...props }): JSX.Element => {
+const Button: React.FC<UnStyledButtonProps> = ({ children, ...props }): JSX.Element => {
 	return <button {...props}>{children}</button>;
 };
 
