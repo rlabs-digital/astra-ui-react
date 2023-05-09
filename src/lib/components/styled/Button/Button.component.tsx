5565eb36a8a1f373e5e24a2568aadbf37893a73e
Rishu Patel <58680469+iamrishupatel@users.noreply.github.com>
2023-05-09 19:02:29 +0530

    sync dev with main (#3)
    * add storybook addon a11y
    
    * setup husky with eslint
    
    * add .npmrc to automatically install peer deps

--- src/lib/components/styled/Button/Button.component.tsx
+++ src/lib/components/styled/Button/Button.component.tsx
@@ -1,10 +1,15 @@
 import { ButtonProps } from '$types/Button';
 import { FC } from 'react';
 import BaseButton from '$base/Button.component';
+import styles from './Button.module.scss';
 
 const Button: FC<ButtonProps> = ({ children, variant, ...props }): JSX.Element => {
 	console.log(variant);
-	return <BaseButton {...props}>{children}</BaseButton>;
+	return (
+		<BaseButton className={styles.btn} {...props}>
+			{children}
+		</BaseButton>
+	);
 };
 
 export default Button;
